3da3d7fe63b4611763782b540d370fd1591cfc6e
Sheikh-JamirAlam <alamjamir15@gmail.com>
2025-01-07 12:48:48 +0530

    Fix button click

--- app/components/VideoArrow.tsx
+++ app/components/VideoArrow.tsx
@@ -6,7 +6,7 @@ export default function VideoArrow() {
   return (
     <div
       onClick={(e) => {
-        document.getElementById("opods-animation-section")?.scrollIntoView({ behavior: "smooth" });
+        document.getElementById("opods")?.scrollIntoView({ behavior: "smooth" });
         e.preventDefault();
       }}
       className="w-14 h-14 sm:w-16 sm:h-16 lg:w-12 lg:h-12 mt-auto flex justify-center items-center text-2xl lg:text-xl bg-light-black/[0.4] rounded-full transition-all cursor-pointer before:absolute before:h-6 before:w-6 before:rounded-full before:bg-light-black/[0] before:duration-300 before:ease-out hover:before:bg-light-black hover:before:h-12 hover:before:w-12"
