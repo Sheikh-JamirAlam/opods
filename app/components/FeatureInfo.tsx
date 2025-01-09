5cb73966fe1c91aee5ffafc69741e71b3bb89139
Sheikh-JamirAlam <alamjamir15@gmail.com>
2025-01-09 22:15:47 +0530

    Responsive modal

--- app/components/FeatureInfo.tsx
+++ app/components/FeatureInfo.tsx
@@ -9,7 +9,7 @@ function useInfoAnimation(isOpen: boolean) {
 
   useEffect(() => {
     const menuAnimations: Segment[] = isOpen
-      ? [[scope.current, { transform: "translateX(10%)", opacity: 1 }, { ease: [0.08, 0.65, 0.53, 0.96], duration: 0.6 }]]
+      ? [[scope.current, { transform: window.innerWidth >= 1024 ? "translateX(10%)" : "translateX(0%)", opacity: 1 }, { ease: [0.08, 0.65, 0.53, 0.96], duration: 0.6 }]]
       : [[scope.current, { transform: "translateX(100%)", opacity: 0 }, { at: "-0.1", duration: 0.4 }]];
 
     animate([...menuAnimations]);
@@ -31,8 +31,8 @@ export default function FeatureInfo({
   const scope = useInfoAnimation(isOpen);
 
   return (
-    <div ref={scope} className="w-full h-screen p-4 bg-pure-white flex justify-between fixed top-0 right-0 z-50 translate-x-[100%]">
-      <ul className="w-[50%] p-10 flex flex-col gap-10 text-foreground">
+    <div ref={scope} className="w-full h-screen p-4 bg-pure-white flex flex-col lg:flex-row justify-between fixed top-0 right-0 z-50 translate-x-[100%]">
+      <ul className="lg:w-[50%] p-4 xs:p-10 flex flex-col gap-10 text-foreground">
         <li
           onClick={() => setIsOpen((prev) => !prev)}
           aria-label="Next button, click to see the other features"
@@ -40,10 +40,13 @@ export default function FeatureInfo({
         >
           <Arrow className="-rotate-90" />
         </li>
-        <li className="text-4xl">{featureTexts.title}</li>
-        <li className="text-4xl">{featureTexts.desc}</li>
+        <li className="text-lg 2xs:text-xl sm:text-2xl lg:text-3xl xl:text-4xl">{featureTexts.title}</li>
+        <li className="text-lg 2xs:text-xl sm:text-2xl lg:text-3xl xl:text-4xl">{featureTexts.desc}</li>
       </ul>
-      <div style={{ backgroundImage: featureTexts.imgUrl, backgroundSize: "cover", backgroundPosition: "center" }} className="w-[40%] h-full mr-[10%] rounded-[30px] shadow-outer-2xl" />
+      <div
+        style={{ backgroundImage: featureTexts.imgUrl, backgroundSize: "cover", backgroundPosition: "center" }}
+        className="xs:w-[70%] md:w-[50%] lg:w-[40%] h-full m-4 xs:m-10 xs:mt-0 xs:mx-auto lg:m-0 lg:mr-[10%] rounded-[30px] shadow-outer-2xl"
+      />
     </div>
   );
 }
